a55d465204a43664d521ef5d2d4e8faa46c0d7a7
hihinkoland <kamil.slimak.sk@gmail.com>
2023-02-07 17:35:02 +0100

    Removed debug rendering

--- Assets/Scripts/PerspectivePan.cs
+++ Assets/Scripts/PerspectivePan.cs
@@ -5,11 +5,8 @@ using UnityEngine;
 public class PerspectivePan : MonoBehaviour
 {
 	private Vector3 touchStart;
-	private Vector3 touchStartRaw;
 	public float zoomMin = 1;
 	public float zoomMax = 8;
-	public GameObject circle;
-	public GameObject center;
 
 	// Start is called before the first frame update
 	void Start()
@@ -24,7 +21,7 @@ public class PerspectivePan : MonoBehaviour
 		if (Input.GetMouseButton(0))
 		{
 			Vector3 direction = new Vector3();
-			if(Input.touchCount == 2)
+			if (Input.touchCount == 2)
 			{
 				Touch touchFirst = Input.GetTouch(0);
 				Touch touchSecond = Input.GetTouch(1);
@@ -37,36 +34,35 @@ public class PerspectivePan : MonoBehaviour
 					float prevMagnitude = (touchFirstPrev - touchSecondPrev).magnitude;
 					float currentMagnitude = (touchFirst.position - touchSecond.position).magnitude;
 					float difference = currentMagnitude - prevMagnitude;
-					zoom(/*Mathf.Log10*/(difference * 0.01f));
+					zoom(difference * 0.01f);
 					// Calculate pinch move
 					Vector2 currentMid = (touchFirst.position + touchSecond.position) / 2;
-					DrawPoint(currentMid, circle);
 					direction = touchStart - Camera.main.ScreenToWorldPoint(currentMid);
-				} else
+				}
+				else
 				{
 					// Init pinch
 					touchStart = Camera.main.ScreenToWorldPoint((touchFirst.position + touchSecond.position) / 2);
-					touchStartRaw = (touchFirst.position + touchSecond.position) / 2;
 					currentMode = PinchMode.pinch;
 				}
-			} else if (Input.touchCount < 2)
+			}
+			else if (Input.touchCount < 2)
 			{
-				if(currentMode == PinchMode.move)
+				if (currentMode == PinchMode.move)
 				{
 					// Calculate move
 					direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-					DrawPoint(Input.mousePosition, circle);
-				} else
+				}
+				else
 				{
 					// Init move
 					touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-					touchStartRaw = Input.mousePosition;
 					currentMode = PinchMode.move;
 				}
 			}
 			// Move cam
 			Camera.main.transform.position += direction;
-			DrawPoint(touchStartRaw, center);
+			// DrawPoint(touchStartRaw, center);
 		}
 		else
 		{
@@ -76,13 +72,9 @@ public class PerspectivePan : MonoBehaviour
 
 	void zoom(float zoom)
 	{
-		Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - zoom, zoomMin, zoomMax);
+		Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - zoom /* Mathf.Min(Mathf.Log10(Camera.main.orthographicSize), 0.1f)*/, zoomMin, zoomMax);
 	}
 
-	void DrawPoint(Vector2 point, GameObject obj)
-	{
-		obj.transform.position = point;
-	}
 	enum PinchMode
 	{
 		none,
