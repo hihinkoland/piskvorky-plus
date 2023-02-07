6d566b9179381ffe68ba31caad5961cbe0053465
hihinkoland <kamil.slimak.sk@gmail.com>
2023-02-07 22:09:30 +0100

    Added auto camera setting, Added easing functions class from: https://gist.github.com/cjddmut/d789b9eb78216998e95c

--- Assets/Scripts/Camera/CameraControl.cs
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+    public Bounds bounds;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (bounds == null)
+            bounds = new Bounds(new Vector3(0, 0, 0), new Vector3(5, 5, 0));
+		(Vector3 center, float size) = CalculateCameraSize();
+        Camera.main.transform.position = center;
+        Camera.main.orthographicSize = size;
     }
 
     // Update is called once per frame
@@ -18,7 +23,6 @@ public class CameraControl : MonoBehaviour
 
     public (Vector3 center, float size) CalculateCameraSize()
     {
-        Bounds bounds = new Bounds();
         foreach (Collider2D col in FindObjectsOfType<Collider2D>()) bounds.Encapsulate(col.bounds);
 
         bounds.Expand(1f);
@@ -26,7 +30,7 @@ public class CameraControl : MonoBehaviour
         float vertical = bounds.size.y;
 		float horizontal = bounds.size.x / Camera.main.aspect;
 
-        float size = Mathf.Min(horizontal, vertical) * 0.5f;
+        float size = Mathf.Max(horizontal, vertical) * 0.5f;
         Vector3 center = bounds.center + new Vector3(0, 0, -10);
         return (center, size);
     }
